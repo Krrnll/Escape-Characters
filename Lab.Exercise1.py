896753912e8fb951db457c9c3f4fb48d7a1763f9
Krrnll <krisrainiell.basco@gmail.com>
2025-11-08 08:26:47 +0800

    Initial Commit

---
+++ Lab.Exercise1.py
@@ -0,0 +1,11 @@
+# Kris Rainiell D. Basco
+# October 16, 2025
+# Python Escape Characters
+
+
+print ('\t\t\t\t\t\t\tI\n\t\t\t\t\t\t\b\t\t\b\bThink\n\t\t\t\t\t\t\t\b\b\b\b\bThat I Shall\n\t\t\t\t\t\t\t\b\b\b\b\b\b\bNever see a poem')
+print ('\t\t\t\t\t\t\t\b\b\b\b\b\b\b\b\bAs lovely as a tree,\n\t\t\t\t\t\t\t\b\b\b\b\b\b\b\b\b\b\b\bA tree whose hungry mouth\n\t\t\t\t\t\t\t\b\b\b\b\b\b\b\b\b\b\b\b\b\bIs pressed against the earth\'s')
+print ('\t\t\t\t\t\t\t\b\b\b\b\b\b\b\b\b\b\b\b\b\b\bSweet flowing breast, a tree that\n\t\t\t\t\t\t\t\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\bLooks at God all day, and lifts it\'s\n\t\t\t\t\t\t\t\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\bLeafty arms to pray. A tree that may in')
+print ('\t\t\t\t\t\t\t\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\bSummer wear, a nest of robin in her hair;\n\t\t\t\t\t\t\t\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\bUpon whose bosom snow has lain; who intimately')
+print ('\t\t\t\t\t\t\t\b\b\b\b\b\bLives with rain.\n\t\t\t\t\t\t\t\b\b\b\b\b\b\bPoems are made by\n\t\t\t\t\t\t\t\b\b\b\b\bFools like me\n\t\t\t\t\t\t\t\bBut')
+print ('\t\t\t\t\t\t\t\bOnly\n\t\t\t\t\t\t\t\bGod\n\t\t\t\t\t\t\t\bCan\n\t\t\t\t\t\t\t\bMake\n\t\t\t\t\t\t\tA\n\t\t\t\t\t\t\t\bTree\n\t\t\t\t\t\t\t\b\b\b\b\bJOYCE KILMER')
